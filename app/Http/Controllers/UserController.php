f24185e8469777aada3d22b4619669e5ea4cb96a
Hemmat <mmhemat99@gmail.com>
2022-03-21 22:29:35 +0200

    merge main

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -52,7 +52,7 @@ class UserController extends Controller
             $imagename=pathinfo($completeimagename,PATHINFO_FILENAME);
             $extension=$request->file('image')->getClientOriginalExtension();
             $compic=str_replace(' ','_',$imagename).'-'.rand().'_'.time().'.'.$extension;
-            $path=$request->file('image')->storeAs('public/images',$compic);
+            $path=$request->file('image')->move('public/images',$compic);
         }
       $user = User::create([
                    'name' => $validatedData['name'],
@@ -213,7 +213,7 @@ try{
             $imagename=pathinfo($completeimagename,PATHINFO_FILENAME);
             $extension=$request->file('image')->getClientOriginalExtension();
             $compic=str_replace(' ','_',$imagename).'-'.rand().'_'.time().'.'.$extension;
-            $path=$request->file('image')->storeAs('public/images',$compic);
+            $path=$request->file('image')->move('public/images',$compic);
         }
 
                $user=User::find($request->user()->id);
